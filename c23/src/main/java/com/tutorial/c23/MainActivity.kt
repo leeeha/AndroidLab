3af6a57c87be0a0f15c6965f6c4e538eb51fd3ce
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-03 22:01:41 +0900

    Touch Event (DOWN, UP, MOVE)

--- c23/src/main/java/com/tutorial/c23/MainActivity.kt
+++ c23/src/main/java/com/tutorial/c23/MainActivity.kt
@@ -1,31 +1,31 @@
 package com.tutorial.c23
 
 import android.os.Bundle
-import android.view.KeyEvent
-import android.widget.Toast
+import android.view.MotionEvent
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    var initTime = 0L
+    lateinit var resultView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultView = findViewById(R.id.resultView)
     }
 
-    // 3초 이내에 백 버튼이 2번 눌리면 앱 종료하기
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if(keyCode == KeyEvent.KEYCODE_BACK){
-            // 3초보다 더 걸리면 토스트 메시지 띄우기
-            if(System.currentTimeMillis() - initTime > 3000){
-                Toast.makeText(this, "종료하려면 한번 더 누르세요!",
-                    Toast.LENGTH_SHORT).show()
-                initTime = System.currentTimeMillis() // 시간 업데이트
-                return true // 키 이벤트에 대한 처리 무시
-            }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var eventType = ""
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> eventType = "DOWN EVENT"
+            MotionEvent.ACTION_UP -> eventType = "UP EVENT"
+            MotionEvent.ACTION_MOVE -> eventType = "MOVE EVENT"
         }
 
-        // 3초보다 적게 걸리면 백 버튼 기능이 적용되어 앱 종료됨.
-        return super.onKeyDown(keyCode, event)
+        resultView.text = "$eventType : (${event?.x},  ${event?.y})"
+
+        return super.onTouchEvent(event)
     }
 }
