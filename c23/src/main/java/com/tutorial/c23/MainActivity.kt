e643acaecf48846bac88e89889f1f268a75aca92
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-03 21:49:44 +0900

    onKeyDown 리턴값이 true면 키 이벤트에 대한 처리 무시

--- c23/src/main/java/com/tutorial/c23/MainActivity.kt
+++ c23/src/main/java/com/tutorial/c23/MainActivity.kt
@@ -16,14 +16,16 @@ class MainActivity : AppCompatActivity() {
     // 3초 이내에 백 버튼이 2번 눌리면 앱 종료하기
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if(keyCode == KeyEvent.KEYCODE_BACK){
+            // 3초보다 더 걸리면 토스트 메시지 띄우기
             if(System.currentTimeMillis() - initTime > 3000){
                 Toast.makeText(this, "종료하려면 한번 더 누르세요!",
                     Toast.LENGTH_SHORT).show()
-                initTime = System.currentTimeMillis()
-                return true
+                initTime = System.currentTimeMillis() // 시간 업데이트
+                return true // 키 이벤트에 대한 처리 무시
             }
         }
 
+        // 3초보다 적게 걸리면 백 버튼 기능이 적용되어 앱 종료됨.
         return super.onKeyDown(keyCode, event)
     }
 }
