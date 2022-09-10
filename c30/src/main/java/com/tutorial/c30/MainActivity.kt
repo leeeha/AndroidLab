5d25ae919b899c7a55e21ded4611f7711d469a6c
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-10 14:12:26 +0900

    ArrayAdapter

---
+++ c30/src/main/java/com/tutorial/c30/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.tutorial.c30
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.main_list)
+        val data = resources.getStringArray(R.array.locations)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            data
+        )
+        listView.adapter = adapter
+    }
+}
