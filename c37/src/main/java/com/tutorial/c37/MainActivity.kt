f4ef031c9d714bb6a2a992ea32b7a2fd29f06afd
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-11 17:26:28 +0900

    MenuInflater

--- c37/src/main/java/com/tutorial/c37/MainActivity.kt
+++ c37/src/main/java/com/tutorial/c37/MainActivity.kt
@@ -2,8 +2,6 @@ package com.tutorial.c37
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +11,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.add(0, 0, 0, "menu1")
-        menu?.add(0, 1, 0, "menu2")
+        menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
-        0 -> {
-            Toast.makeText(this, "menu1 click", Toast.LENGTH_SHORT).show()
-            true
-        }
-        1 -> {
-            Toast.makeText(this, "menu2 click", Toast.LENGTH_SHORT).show()
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
     }
 }
