843078d1bd82db5a4efd7c97f42d13a3058a21ce
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-11 19:05:34 +0900

    Activity, Intent

---
+++ c40/src/main/java/com/tutorial/c40/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.tutorial.c40
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
