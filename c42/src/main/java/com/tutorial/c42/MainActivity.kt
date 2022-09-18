da9868d820e237a7290dc008c994c040a7cacd6a
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-18 16:10:29 +0900

    C42: Implicit Intent

--- c42/src/main/java/com/tutorial/c42/MainActivity.kt
+++ c42/src/main/java/com/tutorial/c42/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.tutorial.c42
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = "ACTION_DETAIL"
+            intent.data = Uri.parse("http://www.google.com")
+            startActivity(intent)
+        }
     }
 }
