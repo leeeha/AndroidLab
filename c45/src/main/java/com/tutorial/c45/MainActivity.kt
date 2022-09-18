2aef9f74b995c20ea964a387e086959dde92d031
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-18 18:37:38 +0900

    C45: 액티비티의 상태 저장

---
+++ c45/src/main/java/com/tutorial/c45/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.tutorial.c45
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    lateinit var countView: TextView
+    lateinit var editView: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        countView = findViewById(R.id.countView)
+        val button = findViewById<Button>(R.id.button)
+        editView = findViewById(R.id.edit)
+
+        button.setOnClickListener {
+            count++
+            countView.text = "$count"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count", count)
+        outState.putString("edit", editView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("count")
+        countView.text = "$count"
+        editView.setText(savedInstanceState.getString("edit"))
+    }
+}
