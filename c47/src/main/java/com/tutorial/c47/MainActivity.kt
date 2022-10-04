f5cb4a5e4e5ad2d6895ce9ba757d43e2213332a4
leeeha <jxlhe46@seoultech.ac.kr>
2022-10-04 20:46:59 +0900

    C48: AsyncTask로 ANR 문제 해결하기

--- c47/src/main/java/com/tutorial/c47/MainActivity.kt
+++ c47/src/main/java/com/tutorial/c47/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.tutorial.c47
 
+import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
+import android.os.SystemClock
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,9 +12,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var pauseView: ImageView
     lateinit var textView: TextView
 
-    var loopFlag = true
     var isFirst = true
-    var isRun = false
+
+    lateinit var asyncTask: MyAsyncTask
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,58 +26,51 @@ class MainActivity : AppCompatActivity() {
 
         startView.setOnClickListener {
             if(isFirst){
+                asyncTask.isRun = true
+                asyncTask.execute() // 스레드 실행
                 isFirst = false
-                isRun = true
-                thread.start() // 개발자가 정의한 별도의 스레드 시작
             }else{
-                isRun = true
+                asyncTask.isRun = true
             }
         }
 
-        // 정지 버튼 누르면 count가 감소하지 않음.
         pauseView.setOnClickListener {
-            isRun = false
+            asyncTask.isRun = false
         }
+
+        asyncTask = MyAsyncTask() // 객체 생성
     }
 
-    // 메인 스레드에서는 핸들러 객체의 handleMessage 함수 오버라이딩
-    var handler: Handler = object: Handler(){
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            if(msg.what === 1){
-                textView.text = msg.arg1.toString() // count 값 보여주기
-            }else if(msg.what === 2){
-                textView.text = msg.obj as String // Finish 문자열 보여주기
-            }
-        }
-    }
+    inner class MyAsyncTask: AsyncTask<Void?, Int?, String>(){
+        var loopFlag = true
+        var isRun = false
 
-    var thread: Thread = object: Thread(){
-        override fun run() {
-            try{
-                var count = 10
-                while(loopFlag){
-                    sleep(1000) // 1초씩 숫자가 감소하도록
-                    if(isRun){
-                        count--
-
-                        var message = Message() // 메시지 객체 생성
-                        message.what = 1
-                        message.arg1 = count
-                        handler.sendMessage(message) // 핸들러에게 작업 의뢰
-
-                        if(count == 0){
-                            message = Message()
-                            message.what = 2
-                            message.obj = "Finish!!"
-                            handler.sendMessage(message) // 핸들러에게 작업 의뢰
-                            loopFlag = false // 반복문 종료
-                        }
+        // 스레드에 의해 백그라운드에서 처리될 내용을 담는다.
+        override fun doInBackground(vararg p0: Void?): String {
+            var count = 10
+            while(loopFlag){
+                SystemClock.sleep(1000)
+                if(isRun){
+                    count--
+                    publishProgress(count) // onProgressUpdate
+                    if(count == 0){
+                        loopFlag = false
                     }
                 }
-            }catch (e: Exception){
-                e.printStackTrace()
             }
+            return "Finish!!" // onPostExecute
+        }
+
+        // doInBackground에서 publishProgress 함수로 넘긴 값이 전달됨.
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+            textView.text = values[0].toString() // count 값
+        }
+
+        // doInBackground 함수의 최종 결과 값을 받기 위해 사용함.
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            textView.text = result // Finish 문자열
         }
     }
 }
