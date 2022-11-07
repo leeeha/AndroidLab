01ff5778e048cbd646c17f599d92dcf47d9e8c9c
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-07 10:56:57 +0900

    chore: C64 갤러리 앱 주석 추가

--- c64/src/main/java/com/tutorial/c64/MainActivity.kt
+++ c64/src/main/java/com/tutorial/c64/MainActivity.kt
@@ -7,7 +7,6 @@ import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
@@ -19,19 +18,20 @@ class MainActivity : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.image_view)
         val button = findViewById<Button>(R.id.button)
 
-        val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        val launcher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) {
             try {
                 val option = BitmapFactory.Options()
                 option.inSampleSize = 5
 
+                // 컨텐츠 프로바이더에게 제공받은 식별자 값으로 비트맵 객체 생성
                 val inputStream = contentResolver.openInputStream(it.data!!.data!!)
                 val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
                 inputStream!!.close()
 
                 bitmap?.let {
-                    imageView.setImageBitmap(bitmap)
+                    imageView.setImageBitmap(bitmap) // 이미지 설정
                 } ?: let {
                     Toast.makeText(this, "이미지 로딩 실패", Toast.LENGTH_SHORT).show()
                 }
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             intent.type = "image/*"
-            launcher.launch(intent)
+            launcher.launch(intent) // 암시적 인텐트로 갤러리 앱 띄우기
         }
     }
 }
