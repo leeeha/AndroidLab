e8e1f6ffb8a1c70cab384c3876e8e8763e36f7e9
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-07 11:18:06 +0900

    chore: C66 카메라 앱 주석 추가

--- c66/src/main/java/com/tutorial/c66/MainActivity.kt
+++ c66/src/main/java/com/tutorial/c66/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -25,9 +24,10 @@ class MainActivity : AppCompatActivity() {
         val dataButton = findViewById<Button>(R.id.dataButton)
         val fileButton = findViewById<Button>(R.id.fileButton)
 
-        val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        val launcher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ){
+            // 부가 데이터로부터 비트맵 객체 획득
             val bitmap = it.data?.extras?.get("data") as Bitmap
             bitmap.let{
                 imageView.setImageBitmap(bitmap)
@@ -35,14 +35,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         dataButton.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            launcher.launch(intent)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) // 이미지 캡쳐 액션
+            launcher.launch(intent) // 암시적 인텐트로 카메라 앱 띄우기
         }
 
         var filePath = ""
-        val fileLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        val fileLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ){
+            // 이미지 파일로부터 비트맵 객체 생성
             val option = BitmapFactory.Options()
             option.inSampleSize = 3
             val bitmap = BitmapFactory.decodeFile(filePath, option)
@@ -59,12 +60,14 @@ class MainActivity : AppCompatActivity() {
                 ".jpg",
                 storageDir
             )
-            filePath = file.absolutePath
+            filePath = file.absolutePath // 파일 경로명 설정
             val uri = FileProvider.getUriForFile(
                 this,
                 "com.tutorial.c66.fileprovider",
                 file
             )
+
+            // 암시적 인텐트의 부가 데이터로 파일 uri 등록하고 카메라 앱 띄우기
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
             fileLauncher.launch(intent)
