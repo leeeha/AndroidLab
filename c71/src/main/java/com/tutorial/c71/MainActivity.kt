0f75808859312d78f7be744614821aa29efc98db
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-15 00:08:04 +0900

    feat: C72 bindService() 함수

--- c71/src/main/java/com/tutorial/c71/MainActivity.kt
+++ c71/src/main/java/com/tutorial/c71/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.tutorial.c71
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binder: MyService.MyBinder
+
+    private val connection: ServiceConnection = object: ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            binder = p1 as MyService.MyBinder
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,30 +30,23 @@ class MainActivity : AppCompatActivity() {
         val stopButton = findViewById<ImageView>(R.id.stopButton)
 
         startButton.setOnClickListener {
-            val intent = Intent("PLAY_TO_SERVICE")
-            intent.putExtra("mode", "start")
-            sendBroadcast(intent)
-
+            binder.startMusic()
             startButton.isEnabled = false
             stopButton.isEnabled = true
         }
 
         stopButton.setOnClickListener {
-            val intent = Intent("PLAY_TO_SERVICE")
-            intent.putExtra("mode", "stop")
-            sendBroadcast(intent)
-
+            binder.stopMusic()
             startButton.isEnabled = true
             stopButton.isEnabled = false
         }
 
         val intent = Intent(this, MyService::class.java)
-        startService(intent)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        val intent = Intent(this, MyService::class.java)
-        stopService(intent)
+        unbindService(connection)
     }
 }
