06996dfc6ee6a48f0642abeede94d6f97dc1ffb5
leeeha <jxlhe46@seoultech.ac.kr>
2022-12-21 17:35:21 +0900

    feat: C94 툴바 기본

---
+++ c94/src/main/java/com/tutorial/c94/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tutorial.c94
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.tutorial.c94.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
