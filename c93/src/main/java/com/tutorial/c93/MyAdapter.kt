7e33c2e2c1e158e1321f1b0b82cb50adac5eaf34
leeeha <jxlhe46@seoultech.ac.kr>
2022-12-21 17:04:11 +0900

    feat: C93 뷰페이저 기본

---
+++ c93/src/main/java/com/tutorial/c93/MyAdapter.kt
@@ -0,0 +1,17 @@
+package com.tutorial.c93
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    val fragments: List<Fragment> = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
